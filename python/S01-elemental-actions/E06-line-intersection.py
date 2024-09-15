79c0e0f24a58e96d2ce897fa8f34e4b39d6ab98d
Druxorey <druxorey@gmail.com>
2024-09-15 02:57:40 -0400

    feat(python): S01[E06-E08] exercises.

---
+++ python/S01-elemental-actions/E06-line-intersection.py
@@ -0,0 +1,17 @@
+def main():
+    print("\n[========= LINE INTERSECTION =========]\n")
+
+    m1 = float(input("Enter the slope of the first line: "))
+    b1 = float(input("Enter the y-intercept of the first line: "))
+
+    m2 = float(input("Enter the slope of the second line: "))
+    b2 = float(input("Enter the y-intercept of the second line: "))
+
+    xIntersection = (b2 - b1) / (m1 - m2)
+    yIntersection = m1 * xIntersection + b1
+
+    print(f"\nThe intersection point is at ({xIntersection}, {yIntersection}).")
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/S01-elemental-actions/E07-multiplication-table.py
@@ -0,0 +1,21 @@
+def main():
+    print("\n[========= MULTIPLICATION TABLE =========]\n")
+
+    number = int(input("Enter a number: "))
+
+    print("\nMultiplication Table of", number)
+    print("===========================")
+    print(f"1  * {number} = {number * 1}")
+    print(f"2  * {number} = {number * 2}")
+    print(f"3  * {number} = {number * 3}")
+    print(f"4  * {number} = {number * 4}")
+    print(f"5  * {number} = {number * 5}")
+    print(f"6  * {number} = {number * 6}")
+    print(f"7  * {number} = {number * 7}")
+    print(f"8  * {number} = {number * 8}")
+    print(f"9  * {number} = {number * 9}")
+    print(f"10 * {number} = {number * 10}")
+
+
+if __name__ == "__main__":
+    main()
