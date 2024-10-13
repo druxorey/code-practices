19044e355ec8f7cf0b4efb5a49ac5c2b11af19f3
Druxorey <druxorey@gmail.com>
2024-10-13 03:18:26 -0400

    feat(c++): improved the way to randomize the elements of an array.

--- c++/S05-arrays/array-functions.cpp
+++ c++/S05-arrays/array-functions.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
 int randArray(int array[], int size) {
+	srand(time(NULL));
+
 	for (int i = 0; i < size; i++) {
-		array[i] = rand() % size;
+		array[i] = rand() % (size * 2);
 	}
 	return array[size];
 }
+
 
 int fillArray(int array[], int size) {
 	for (int i = 0; i < size; i++) {
@@ -14,10 +17,14 @@ int fillArray(int array[], int size) {
 	return array[size];
 }
 
+
 void printArray(int array[], int size) {
-	std::cout << '\n';
+	std::cout << '\n' << "The array is: [";
 	for (int i = 0; i < size; i++) {
-		std::cout << array[i] << " ";
+		std::cout << array[i];
+		if (i != size - 1) {
+			std::cout << ",";
+		}
 	}
-	std::cout << '\n';
+	std::cout << "]" << '\n';
 }
