362876713d586c4dcb8d82b50e5e929297a859fe
Druxorey <druxorey@gmail.com>
2024-10-13 18:51:01 -0400

    feat(c++): implemented fillMatrix and printMatrix functions.

--- c++/S05-arrays/array-functions.cpp
+++ c++/S05-arrays/array-functions.cpp
@@ -1,20 +1,47 @@
 #include <iostream>
 
-int randArray(int array[], int size) {
+void randArray(int array[], int size) {
 	srand(time(NULL));
 
 	for (int i = 0; i < size; i++) {
 		array[i] = rand() % (size * 2);
 	}
-	return array[size];
 }
 
 
-int fillArray(int array[], int size) {
+void fillArray(int array[], int size) {
 	for (int i = 0; i < size; i++) {
 		array[i] = i;
 	}
-	return array[size];
+}
+
+
+void fillMatrix(int** matrix, int sizeMatrix) {
+    int number = 1;
+
+    for (int i = 0; i < sizeMatrix; i++) {
+        for (int j = 0; j < sizeMatrix; j++) {
+            matrix[i][j] = number + j;
+        }
+        number += sizeMatrix;
+    }
+}
+
+
+void printMatrix(int** matrix, int sizeMatrix) {
+    int biggestNumber = sizeMatrix * sizeMatrix;
+    int digits = std::to_string(biggestNumber).length();
+
+    for (int i = 0; i < sizeMatrix; i++) {
+        for (int j = 0; j < sizeMatrix; j++) {
+            int currentDigits = std::to_string(matrix[i][j]).length();
+            for (int k = 0; k < digits - currentDigits; k++) {
+                std::cout << " ";
+            }
+            std::cout << matrix[i][j] << "|";
+        }
+        std::cout << '\n';
+    }
 }
 
 
