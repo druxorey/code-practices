3f09eedb5b40408e83d9bdb4fd270e6a8fc99027
Druxorey <druxorey@gmail.com>
2024-10-14 21:31:06 -0400

    feat(c++): printArray() use a template to print any array.

--- c++/U1-libraries/dxarray.cpp
+++ c++/U1-libraries/dxarray.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+template <typename datatype>
+void printArray(datatype array[], int size) {
+	std::cout << '\n' << "The array is: [";
+	for (int i = 0; i < size; i++) {
+		std::cout << array[i];
+		if (i != size - 1) {
+			std::cout << ",";
+		}
+	}
+	std::cout << "]" << '\n';
+}
+
+
 void randArray(int array[], int size) {
 	srand(time(NULL));
 
@@ -13,16 +26,4 @@ void fillArray(int array[], int size) {
 	for (int i = 0; i < size; i++) {
 		array[i] = i;
 	}
-}
-
-
-void printArray(int array[], int size) {
-	std::cout << '\n' << "The array is: [";
-	for (int i = 0; i < size; i++) {
-		std::cout << array[i];
-		if (i != size - 1) {
-			std::cout << ",";
-		}
-	}
-	std::cout << "]" << '\n';
 }
