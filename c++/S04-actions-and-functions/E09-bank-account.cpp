c3f265036e228ec0b274b493a9a72fa18d56effe
Druxorey <druxorey@gmail.com>
2024-10-14 21:36:15 -0400

    fix(c++): S04[E09] exercises.

--- c++/S04-actions-and-functions/E09-bank-account.cpp
+++ c++/S04-actions-and-functions/E09-bank-account.cpp
@@ -70,9 +70,7 @@ int main() {
 				std::cout << "Invalid choise" << '\n';
 		}
 
-		std::cout << "\nPress enter to continue..";
-		std::cin.get(); // Wait for user to press enter
-		std::cin.get(); // It is necessary to use two times bc the first one doesn't  get the \n character
+		enter();
 
 	} while (choise != 4);
 
