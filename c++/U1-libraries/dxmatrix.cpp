d6081cf27ba96ca505dc2a9bff4a92943729d854
Druxorey <druxorey@gmail.com>
2024-10-14 21:31:56 -0400

    feat(c++): printMatrix() use a template to print any matrix.

--- c++/U1-libraries/dxmatrix.cpp
+++ c++/U1-libraries/dxmatrix.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
 
-void fillMatrix(int** matrix, int sizeMatrix) {
-    int number = 1;
-
-    for (int i = 0; i < sizeMatrix; i++) {
-        for (int j = 0; j < sizeMatrix; j++) {
-            matrix[i][j] = number + j;
-        }
-        number += sizeMatrix;
-    }
-}
-
-
-void printMatrix(int** matrix, int sizeMatrix) {
+template <typename datatype>
+void printMatrix(datatype** matrix, int sizeMatrix) {
     int biggestNumber = sizeMatrix * sizeMatrix;
     int digits = std::to_string(biggestNumber).length();
 
@@ -28,3 +17,14 @@ void printMatrix(int** matrix, int sizeMatrix) {
     }
 }
 
+
+void fillMatrix(int** matrix, int sizeMatrix) {
+    int number = 1;
+
+    for (int i = 0; i < sizeMatrix; i++) {
+        for (int j = 0; j < sizeMatrix; j++) {
+            matrix[i][j] = number + j;
+        }
+        number += sizeMatrix;
+    }
+}
