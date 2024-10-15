1c4bfa727730f38e93bc243fbb438c4f1e5c5967
Druxorey <druxorey@gmail.com>
2024-10-15 00:38:04 -0400

    fix(c++): S05[E14] exercises.

--- c++/S05-arrays/E14-pharmacy-value.cpp
+++ c++/S05-arrays/E14-pharmacy-value.cpp
@@ -7,7 +7,7 @@ double getProductValue (int cuantityList[], float priceList[], int index){
 }
 
 
-void showProducts (std::string nameList[], int cuantityList[], int size) {
+void showProducts (int cuantityList[], int size) {
 	int total = 0;
 
 	for (int i = 0; i < size; i++)
@@ -27,12 +27,31 @@ void showTotalValue (int cuantityList[], float priceList[], int size) {
 }
 
 
+void sortProducts(int cuantityList[], float priceList[], int size) {
+	int tempCuantity;
+	float tempPrice;
+
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = 0; j < size - i - 1; j++) {
+			if (cuantityList[j] > cuantityList[j + 1]) {
+				tempCuantity = cuantityList[j];
+				cuantityList[j] = cuantityList[j + 1];
+				cuantityList[j + 1] = tempCuantity;
+
+				tempCuantity = priceList[j];
+				priceList[j] = priceList[j + 1];
+				priceList[j + 1] = tempCuantity;
+			}
+		}
+	}
+}
+
+
 int main() {
-	std::string nameList[10] = {"Paracetamol", "Panadol", "Vitamin C", "Vitamin D", "Vitamin E", "Vitamin B", "Vitamin A", "Vitamin K", "Vitamin B12", "Vitamin B6"};
 	int cuantityList[10] = {120, 134, 319, 153, 91, 68, 73, 82, 90, 115};
 	float priceList[10] = {2.50, 3.00, 4.50, 5.20, 6.00, 7.75, 8.00, 9.50, 10.00, 11.30};
 
-	int option;
+	int option, listSize = sizeof(cuantityList) / sizeof(cuantityList[0]);
 
 	do {
 		system("clear");
@@ -41,24 +60,32 @@ int main() {
 		printf("  1) Total products\n");
 		printf("  2) Total stock value\n");
 		printf("  3) Total stock value by product\n");
-		printf("  4) Exit\n");
+		printf("  4) Sort by cuantity\n");
+		printf("  5) Exit\n");
 
 		getInput("\nOption: ", option);
 
 		system("clear");
 		switch (option) {
 			case 1:
-				showProducts(nameList, cuantityList, sizeof(nameList) / sizeof(nameList[0]));
+				showProducts(cuantityList, listSize);
 				break;
 			case 2:
-				showTotalValue(cuantityList, priceList, sizeof(nameList) / sizeof(nameList[0]));
+				showTotalValue(cuantityList, priceList, listSize);
 				break;
 			case 3:
 				getInput("Enter the product number: ", option);
-				printf("\n\e[1;35mPRODUCT:\e[0m %s", nameList[option - 1].c_str());
 				printf("\n\e[1;35mPRODUCT VALUE:\e[0m %.2f$\n", getProductValue(cuantityList, priceList, option - 1));
 				break;
 			case 4:
+				printf("\n\e[1;35mINITIAL ARRAY\e[0m\n");
+				printArray(cuantityList, listSize);
+
+				printf("\n\e[1;35mSORTED ARRAY\e[0m\n");
+				sortProducts(cuantityList, priceList, listSize);
+				printArray(cuantityList, listSize);
+				break;
+			case 5:
 				printf("Exiting...\n");
 				break;
 			default:
@@ -67,7 +94,7 @@ int main() {
 		}
 		enter();
 
-	} while (option != 4);
+	} while (option != 5);
 
 	return 0;
 }
