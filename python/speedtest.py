5e26364e0b18ad6b8b7ea6b281497d73cce320ba
Druxorey <druxorey@gmail.com>
2024-10-24 01:22:52 -0400

    feat(speedtest): improved python test outputs.

--- python/speedtest.py
+++ python/speedtest.py
@@ -1,19 +1,36 @@
 import time
 
+# The average execution time of this script is 3928.1 ms after 10 test
+
+ITERATIONS: int = 10
+NUMBER: int = 100_000_000
+
+def speedtest() -> float:
+	start: float = time.time() * 1000
+
+	count = 0
+	for _ in range (NUMBER):
+		count += 1
+
+	duration: float = round(time.time() * 1000 - start)
+
+	return duration
+
 
 def main():
-    start = time.time() * 1000
+	averageExecutionTime: float = 0
 
-    count = 0
-    number = 50_000_000
+	print("Starting speed test...")
 
-    for i in range (number):
-        count += 1
+	for i in range(ITERATIONS):
+		finalDuration: float = speedtest()
+		averageExecutionTime += finalDuration
+		print(f"{i + 1}. Counting to {NUMBER} in Python took {finalDuration} ms")
+	
+	averageExecutionTime /= ITERATIONS
 
-    duration = int(round(time.time() * 1000 - start))
-
-    print(f"Counting to {number} in Python took {duration} ms.")
+	print(f"\n\033[32mAverage execution time: {averageExecutionTime} ms.")
 
 
 if __name__ == "__main__":
-    main()
+	main()
