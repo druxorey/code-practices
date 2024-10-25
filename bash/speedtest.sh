7855dd2ce50b69ba8e4308d3408b4a04aa659762
Druxorey <druxorey@gmail.com>
2024-10-24 21:54:41 -0400

    feat(speedtest): improved bash test outputs.

--- bash/speedtest.sh
+++ bash/speedtest.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
-function main(){
+# The average execution time of this script is 261204 ms (4.35 min) after 10 test
 
+ITERATIONS=10
+NUMBER=100000000
+
+function speedtest() {
     start=$(date +%s%N)
 
     count=0
-    number=50000000
-
-    for (( i=0; i<=number; i++ )); do
-       true 
+    for (( i = 0; i <= NUMBER; i++ )); do
+		count=$((count + 1))
     done
 
     end=$(date +%s%N)
-    time=$(( (end - start) / 1000000 ))
 
-    echo "Counting to number $number in Bash took $time ms."
+    duration=$(( (end - start) / 1000000 ))
+    echo $duration
+}
+
+
+function main(){
+	averageExecutionTime=0
+
+	echo "Starting speed test..."
+
+	for (( i = 0; i < ITERATIONS; i++ )); do
+		finalDuration=$(speedtest)
+		averageExecutionTime=$((averageExecutionTime + finalDuration))
+		echo -e "$((i + 1)). Counting to number $NUMBER in Bash took $finalDuration ms"
+	done
+
+	averageExecutionTime=$((averageExecutionTime / ITERATIONS))
+
+	echo -e "\n\033[32mAverage execution time: $averageExecutionTime ms."
 }
 
 main bash
