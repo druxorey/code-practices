0f754cf14591f2ce2f7874af3524990ce53fea6f
Druxorey <druxorey@gmail.com>
2024-11-03 03:14:20 -0400

    feat(c++): S08[E03] exercise.

---
+++ c++/S08-recursion/E03-recursive-fibonacci.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <unordered_map>
+#include "../U1-libraries/dxinput.cpp"
+
+typedef unsigned long long int ulli;
+
+ulli getFibonnacci(ulli n, std::unordered_map<ulli, ulli>& memo) {
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+	if (memo.find(n) != memo.end()) return memo[n];
+
+	memo[n] = getFibonnacci(n - 1, memo) + getFibonnacci(n - 2, memo);
+
+	return memo[n];
+}
+
+int main() {
+	ulli number, fibonacci;
+	std::unordered_map<ulli, ulli> memo;
+
+	std::cout << "\n\e[0;35m[========= RECURSIVE FIBONACCI =========]\e[0m\n" << '\n';
+
+	getInput("Enter a number: ", number);
+
+	if (number > 199) {
+		printf("Fibonacci of %lld is too large to be calculated\n", number);
+		return 1;
+	}
+
+	printf("Fibonacci of %d is %lld\n", number, getFibonnacci(number, memo));
+
+	return 0;
+}
