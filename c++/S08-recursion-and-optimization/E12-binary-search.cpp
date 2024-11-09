f9e175b466e71a26b00bc1783c633a0cd8916dd2
Druxorey <druxorey@gmail.com>
2024-11-09 18:44:59 -0400

    feat(c++): S08[E12] exercise.

---
+++ c++/S08-recursion-and-optimization/E12-binary-search.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "../U1-libraries/dxinput.cpp"
+#include "../U1-libraries/dxarray.cpp"
+
+int binarySearch(int array[], int number, int top, int bottom = 0) {
+	int middle = (top + bottom) / 2;
+	if (array[middle] == number) return middle;
+	if (top <= bottom) return -1;
+	if (array[middle] < number) return binarySearch(array, number, top, middle + 1);
+	return binarySearch(array, number, middle - 1, bottom);
+}
+
+
+int main() {
+	int size, number, position;
+
+	std::cout << "\n\e[0;35m[========= BINARY SEARCH =========]\e[0m\n" << '\n';
+
+	getInput("Enter the size of the array: ", size);
+	getInput("Enter the number to search: ", number);
+	int numbers[size];
+
+	fillArray(numbers, size);
+	printArray(numbers, size);
+
+	position = binarySearch(numbers, number, size);
+
+	if (position == -1)
+		printf("\n\e[0;31mThe number was not found.\e[0m\n");
+	else
+		printf("\n\e[0;32mThe number was found at position %d.\e[0m\n", position);
+
+	return 0;
+}
