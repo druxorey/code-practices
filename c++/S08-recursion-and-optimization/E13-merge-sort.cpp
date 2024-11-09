0f4b324f2a82711fd699736b43037099ffd27538
Druxorey <druxorey@gmail.com>
2024-11-09 19:48:16 -0400

    feat(c++): S08[E13] exercise.

---
+++ c++/S08-recursion-and-optimization/E13-merge-sort.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "../U1-libraries/dxinput.cpp"
+#include "../U1-libraries/dxarray.cpp"
+
+void mergeSort(int array[], int size) {
+	if (size < 2) return;
+
+	int middle = size / 2;
+	int left[middle], right[size - middle];
+
+	for (int i = 0; i < middle; i++) left[i] = array[i];
+	for (int i = middle; i < size; i++) right[i - middle] = array[i];
+
+	mergeSort(left, middle);
+	mergeSort(right, size - middle);
+
+	int i = 0, j = 0, k = 0;
+
+	while (i < middle && j < size - middle) {
+		if (left[i] < right[j]) array[k++] = left[i++];
+		else array[k++] = right[j++];
+	}
+
+	while (i < middle) array[k++] = left[i++];
+	while (j < size - middle) array[k++] = right[j++];
+}
+
+
+int main() {
+	int size, position;
+
+	std::cout << "\n\e[0;35m[========= MERGE SORT =========]\e[0m\n" << '\n';
+
+	getInput("Enter the size of the array: ", size);
+	int numbers[size];
+
+	randArray(numbers, size);
+	printf("\n\e[0;33mGenerated array:\e[0m\n");
+	printArray(numbers, size);
+
+	mergeSort(numbers, size);
+	printf("\n\e[0;33mSorted array:\e[0m\n");
+	printArray(numbers, size);
+
+	return 0;
+}
