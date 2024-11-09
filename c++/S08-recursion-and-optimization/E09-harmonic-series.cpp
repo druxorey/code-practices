fa7580c2e18877269947a4528493073aa7802243
Druxorey <druxorey@gmail.com>
2024-11-09 15:43:33 -0400

    feat(c++): S08[E09] exercise.

---
+++ c++/S08-recursion-and-optimization/E09-harmonic-series.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "../U1-libraries/dxinput.cpp"
+
+double getHarmonic(int size) {
+	if (size <= 1) return 1.0;
+	return (1.0 / (double)size) + getHarmonic(size - 1);
+}
+
+
+int main() {
+	int size = 0;
+
+	std::cout << "\n\e[0;35m[========= HARMONIC SERIES =========]\e[0m\n" << '\n';
+
+	getInput("Enter the number of terms to sum: ", size);
+	printf("\e[0;32mThe sum of the first %d terms of the harmonic series is %f\e[0m\n", size, getHarmonic(size));
+
+	return 0;
+}
