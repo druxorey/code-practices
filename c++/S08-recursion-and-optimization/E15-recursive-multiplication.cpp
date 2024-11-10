861e7bbb098eaa5caf754dbc4860e904720abe70
Druxorey <druxorey@gmail.com>
2024-11-09 20:23:36 -0400

    feat(c++): S08[E15] exercise.

---
+++ c++/S08-recursion-and-optimization/E15-recursive-multiplication.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "../U1-libraries/dxinput.cpp"
+#include "../U1-libraries/dxarray.cpp"
+
+int multiply(int a, int b) {
+	if (b == 0) return 0;
+	return a + multiply(a, b - 1);
+}
+
+
+int main() {
+	int a = 0, b = 0;
+
+	std::cout << "\n\e[0;35m[========= RECURSIVE MULTIPLICATION =========]\e[0m\n" << '\n';
+
+	getInput("Enter the first number: ", a);
+	getInput("Enter the second number: ", b);
+
+	printf("\n\e[0;33mThe result of %d * %d is %d\e[0m\n", a, b, multiply(a, b));
+
+	return 0;
+}
