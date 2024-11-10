b54d869b4df0a0898c32829461cc679c60be20ce
Druxorey <druxorey@gmail.com>
2024-11-09 21:13:36 -0400

    feat(c++): S08[E16] exercise.

---
+++ c++/S08-recursion-and-optimization/E16-parity.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "../U1-libraries/dxinput.cpp"
+#include "../U1-libraries/dxarray.cpp"
+
+bool isOdd(int n);
+bool isEven(int n);
+
+
+bool isOdd(int n) {
+	if (n == 0) return false;
+	return isEven(n - 1);
+}
+
+
+bool isEven(int n) {
+	if (n == 0) return true;
+	return isOdd(n - 1);
+}
+
+
+int main() {
+	int a = 0;
+
+	std::cout << "\n\e[0;35m[========= PARITY =========]\e[0m\n" << '\n';
+
+	getInput("Enter the first number: ", a);
+	printf("\n\e[0;32m%d\e[0m is %s\n", a, isEven(a) ? "even" : "odd");
+
+	return 0;
+}
