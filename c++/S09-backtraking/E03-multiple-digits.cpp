9314b92985c54cf36e130ad5982acc1a7595fbf4
Druxorey <druxorey@gmail.com>
2024-11-16 16:26:44 -0400

    feat(c++): S09[E03] exercise.

---
+++ c++/S09-backtraking/E03-multiple-digits.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include "../U1-libraries/dxinput.cpp"
+
+const int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+bool isValid(int number) {
+	std::string numberStr = std::to_string(number);
+	int length = numberStr.length();
+
+	for (int i = length; i > 1; i--) {
+		if (number % i != 0) return false;
+		number /= 10;
+	}
+
+	return true;
+}
+
+
+void printNumbers(int size, int number = 0, int digitCount[10] = nullptr) {
+	if (number > size) {
+		if (isValid(number)) printf("%d\n", number);
+		return;
+	}
+
+    if (digitCount == nullptr) {
+        int initialDigitCount[10] = {0};
+        digitCount = initialDigitCount;
+    }
+
+    for (int i = 0; i < 9; i++) {
+        int digit = numbers[i];
+        if (digitCount[digit] == 0) {
+            digitCount[digit]++;
+            printNumbers(size, number * 10 + digit, digitCount);
+            digitCount[digit]--;
+        }
+    }
+}
+
+
+int main() {
+	int size, limit = 1;
+
+	std::cout << "\n\e[0;35m[========= MULTIPLE DIGITS =========]\e[0m\n" << '\n';
+
+	getInput("Enter the max digits: ", size);
+
+	while (size - 1 > 0) {
+		limit *= 10;
+		size--;
+	}
+
+	printNumbers(limit);
+
+	return 0;
+}
