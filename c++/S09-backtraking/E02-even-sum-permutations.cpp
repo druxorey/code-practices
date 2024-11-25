9d2a05000d78c9c7a5a3a25f49897cc4ce3c4173
Druxorey <druxorey@gmail.com>
2024-11-25 00:51:19 -0400

    feat(c++): S09[E02] exercise.

---
+++ c++/S09-backtraking/E02-even-sum-permutations.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "../U1-libraries/dxinput.cpp"
+#include "../U1-libraries/dxarray.cpp"
+
+void swap(char &a, char &b) {
+	char temp = a;
+	a = b;
+	b = temp;
+}
+
+
+void getPermutations(char array[], int size, int &permutations, int index = 0) {
+	if (index == size) {
+		int totalSum = 0;
+		for (int i = 0; i < size; i+=2) {
+			totalSum += array[i];
+		}
+
+		if (totalSum % 2 == 0) {
+			printArray(array, size);
+			permutations++;
+		}
+
+		return;
+	}
+
+	for (int i = index; i < size; i++) {
+		swap(array[index], array[i]);
+		getPermutations(array, size, permutations, index + 1);
+		swap(array[index], array[i]);
+	}
+}
+
+
+int main() {
+	int size, permutations = 0;
+
+	std::cout << "\n\e[0;35m[========= N EVEN PERMUTATIONS =========]\e[0m\n" << '\n';
+
+	getInput("Enter the string size: ", size);
+
+	char letters[size];
+
+	fillArray(letters, size);
+	getPermutations(letters, size, permutations);
+	printf("\n\e[0;32mTotal permutations: %d\e[0m\n", permutations);
+
+	return 0;
+}
