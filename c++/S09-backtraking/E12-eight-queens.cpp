9a019170aff07df5c9d5b74a9a7c809d0f6d82a8
Druxorey <druxorey@gmail.com>
2024-12-11 11:49:55 -0400

    refactor(c++): variable names and improve board size validation:
    - renamed variable 'matrix' to 'board' for better clarity.
    - added validation to ensure board size is at least 4.
    - updated printMatrix function call to include custom symbols.

--- c++/S09-backtraking/E12-eight-queens.cpp
+++ c++/S09-backtraking/E12-eight-queens.cpp
@@ -2,31 +2,31 @@
 #include "../U1-libraries/dxinput.cpp"
 #include "../U1-libraries/dxmatrix.cpp"
 
-bool isValid(bool** matrix, int size, int row, int col) {
+bool isValid(bool** board, int size, int row, int col) {
     for (int i = 0; i < row; i++)
-        if (matrix[i][col])
+        if (board[i][col])
             return false;
 
     for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
-        if (matrix[i][j])
+        if (board[i][j])
             return false;
 
     for (int i = row, j = col; i >= 0 && j < size; i--, j++)
-        if (matrix[i][j])
+        if (board[i][j])
             return false;
 
     return true;
 }
 
 
-bool nQueens(bool** matrix, int size, int row = 0, int queens = 0) {
+bool nQueens(bool** board, int size, int row = 0, int queens = 0) {
 	if (queens == size) return true;
 	for (int i = 0; i < size; i++) {
-		if(isValid(matrix, size, row, i)) {
-			matrix[row][i] = true;
+		if(isValid(board, size, row, i)) {
+			board[row][i] = true;
 			queens++;
-			if (nQueens(matrix, size, row + 1, queens)) return true;
-			matrix[row][i] = false;
+			if (nQueens(board, size, row + 1, queens)) return true;
+			board[row][i] = false;
 			queens--;
 		}
 	}
@@ -41,6 +41,11 @@ int main() {
 	int boardSize;
 	getInput("Enter board size: ", boardSize);
 
+	if (boardSize < 4) {
+		printf("\n\e[0;31mThe board size must be greater or equal than 4\e[0m\n");
+		return 1;
+	}
+
 	bool** board = new bool*[boardSize];
 	for (int i = 0; i < boardSize; i++) {
 		board[i] = new bool[boardSize];
@@ -50,7 +55,7 @@ int main() {
 	}
 
 	nQueens(board, boardSize);
-	printMatrix(board, boardSize);
+	printMatrix(board, boardSize, "", " ");
 
 	for (int i = 0; i < boardSize; i++) {
 		delete[] board[i];
