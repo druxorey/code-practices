18b2c08de58bb773c946ad911cdd05173c2491b3
Druxorey <druxorey@gmail.com>
2024-12-12 02:10:42 -0400

    feat(c++): improved obtaining of biggestNumber.

--- c++/U1-libraries/dxmatrix.cpp
+++ c++/U1-libraries/dxmatrix.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+int getBiggestNumber(int** matrix, int size) {
+	int biggestNumber = matrix[size-1][size-1];
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			if (matrix[i][j] > biggestNumber) biggestNumber = matrix[i][j];
+		}
+	}
+	return biggestNumber;
+}
+
+
 template <typename datatype, int rows, int cols>
 void printMatrix(datatype (&matrix)[cols][rows]) {
     int biggestNumber = rows * cols;
@@ -20,7 +31,7 @@ void printMatrix(datatype (&matrix)[cols][rows]) {
 
 template <typename datatype>
 void printMatrix(datatype** matrix, int sizeMatrix) {
-    int biggestNumber = sizeMatrix * sizeMatrix;
+    int biggestNumber = getBiggestNumber(matrix, sizeMatrix);
     int digits = std::to_string(biggestNumber).length();
 
     for (int i = 0; i < sizeMatrix; i++) {
@@ -29,7 +40,7 @@ void printMatrix(datatype** matrix, int sizeMatrix) {
         for (int j = 0; j < sizeMatrix; j++) {
             int currentDigits = std::to_string(matrix[i][j]).length();
 
-            for (int k = 1; k < digits - currentDigits; k++) {
+            for (int k = 0; k < digits - currentDigits; k++) {
                 std::cout << " ";
             }
 			std::cout << matrix[i][j] << "\e[0;34m|\e[0m";
