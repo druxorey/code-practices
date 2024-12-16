d950f5723caacdd28b91bed1d188bf06b4f4b7f8
Druxorey <druxorey@gmail.com>
2024-12-16 02:46:15 -0400

    feat(python): S02[E09] exercise.

---
+++ python/S02-conditional-control-structures/E09-coordinates-converter.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+def main():
+	print("\n\033[35m[========= COORDINATES CONVERTER =========]\033[0m\n")
+
+	print("1. Cartesian to Polar")
+	print("2. Polar to Cartesian")
+
+	option = int(input("Select an option: "))
+
+	if (option == 1):
+		print("\n\033[32m Converting from Cartesian to Polar\033[0m\n")
+
+		r = float(input("Enter the value of r: "))
+		theta = float(input("Enter the value of theta: "))
+
+		theta = np.radians(theta)
+		x = r * np.cos(theta)
+		y = r * np.sin(theta)
+
+		print(f"The result is: x = {x}, y = {y}")
+
+	if (option == 2):
+		print("\n\033[32m Converting from Polar to Cartesian\033[0m\n")
+
+		x = float(input("Enter the value of x: "))
+		y = float(input("Enter the value of y: "))
+
+		theta = np.degrees(np.arctan2(y, x))
+		r = np.hypot(x, y)
+
+		print(f"The result is: radius = {r}, theta = {theta}")
+
+	if (option != 1 and option != 2):
+		print("\033[31mInvalid option.\033[0m")
+
+
+if __name__ == "__main__":
+	main()
