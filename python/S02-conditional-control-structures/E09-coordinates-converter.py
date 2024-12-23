a085d78f8c822dec998ed63e90cda3428ac031b8
Druxorey <druxorey@gmail.com>
2024-12-23 17:33:30 -0400

    fix(python): S02[E09] exercise.

--- python/S02-conditional-control-structures/E09-coordinates-converter.py
+++ python/S02-conditional-control-structures/E09-coordinates-converter.py
@@ -11,6 +11,17 @@ def main():
 	if (option == 1):
 		print("\n\033[32m Converting from Cartesian to Polar\033[0m\n")
 
+		x = float(input("Enter the value of x: "))
+		y = float(input("Enter the value of y: "))
+
+		theta = np.degrees(np.arctan2(y, x))
+		r = np.hypot(x, y)
+
+		print(f"The result is: radius = {r}, theta = {theta}")
+
+	elif (option == 2):
+		print("\n\033[32m Converting from Polar to Cartesian\033[0m\n")
+
 		r = float(input("Enter the value of r: "))
 		theta = float(input("Enter the value of theta: "))
 
@@ -20,18 +31,7 @@ def main():
 
 		print(f"The result is: x = {x}, y = {y}")
 
-	if (option == 2):
-		print("\n\033[32m Converting from Polar to Cartesian\033[0m\n")
-
-		x = float(input("Enter the value of x: "))
-		y = float(input("Enter the value of y: "))
-
-		theta = np.degrees(np.arctan2(y, x))
-		r = np.hypot(x, y)
-
-		print(f"The result is: radius = {r}, theta = {theta}")
-
-	if (option != 1 and option != 2):
+	else:
 		print("\033[31mInvalid option.\033[0m")
 
 
