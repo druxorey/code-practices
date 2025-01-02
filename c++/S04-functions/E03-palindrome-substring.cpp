e89ba01fc395edf1a5cea0b8edc37f85b27c8287
Druxorey <druxorey@gmail.com>
2025-01-02 15:26:19 -0400

    perf(c++): optimize S04[E03] isCapicua function with string input.

--- c++/S04-functions/E03-palindrome-substring.cpp
+++ c++/S04-functions/E03-palindrome-substring.cpp
@@ -1,40 +1,34 @@
 #include <iostream>
 #include "../U1-libraries/dxinput.cpp"
 
-bool isCapicua(int number) {
-	int capicua = 0, comparator = number;
+int isCapicua(std::string number) {
+	int size = number.size();
 
-	do {
-		capicua = capicua * 10 + number % 10;
-		number /= 10;
-	} while (number - 1 != -1);
+	for (int i = 0; i < size - 1; i++) {
+		if (number[i] == number[i + 2]) {
+			std::string capicua = number.substr(i, 3);
+			return std::stoi(capicua);
+		}
+	}
 
-	return capicua == comparator;
+	return -1;
 }
 
 
 int main(int argc, char *argv[]) {
 	std::cout << "\n\e[0;35m[========= PALINDROME SUBSTRING =========]\e[0m\n\n";
 
-	int number = 0;
+	std::string number;
 
-	do {
-		getcin("Enter the number to be evaluated: ", number);
+	getcin("Enter the number to be evaluated: ", number);
 
-		if (!(number < 1000000 && number > 100000))
-			printf("The number must have 6 digits.\n");
+	int capicuaNumber = isCapicua(number);
 
-	} while (!(number < 1000000 && number > 100000));
-
-	for (int i = 0; i <= 3; i++) {
-		int evaluatedNumber = number % 1000;
-		if (isCapicua(evaluatedNumber)) {
-			printf("The number %i is capicua.\n", evaluatedNumber);
-			return 0;
-		}
-		number /= 10;
+	if (capicuaNumber != -1) {
+		printf("The number %i is capicua.\n", capicuaNumber);
+	} else {
+		printf("There isn't any capicua number.\n");
 	}
-	printf("There isn't any capicua number.\n");
 
 	return 0;
 }
