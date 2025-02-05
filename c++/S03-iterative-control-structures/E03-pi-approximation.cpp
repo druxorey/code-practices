e9f5b8c9749760f722b78be65ab18f4afa3038dd
Druxorey <druxorey@gmail.com>
2025-02-05 19:36:52 -0400

    perf(c++): optimize S03[E03 pi approximation calculation.

--- c++/S03-iterative-control-structures/E03-pi-approximation.cpp
+++ c++/S03-iterative-control-structures/E03-pi-approximation.cpp
@@ -3,16 +3,24 @@
 int main(int argc, char *argv[]) {
 	std::cout << "\n\e[0;35m[========= PI APPROXIMATION =========]\e[0m\n\n";
 
-	double inputNumber, piValue = 4;
+	double piValue = 4.0;
+	int totalIterations, i;
 
 	std::cout << "Enter the number of iterations: ";
-	std::cin >> inputNumber;
+	std::cin >> totalIterations;
 
-	for (double i = 3; i <= (inputNumber * 4) ; i += 4) {
-		piValue = piValue - (4/i) + (4/(i+2));
+	if (totalIterations > 100000) {
+		for (i = 3; i <= (totalIterations * 4) ; i += 4) {
+			piValue = piValue - (4 / (double)i) + (4 / ((double)i + 2.0));
+			printf(" \e[0;33mActual calculation:\e[0m %.32lf \n", piValue);
+		}
+	} else {
+		for (i = 3; i <= (totalIterations * 4) ; i += 4) {
+			piValue = piValue - (4 / (double)i) + (4 / ((double)i + 2.0));
+		}
 	}
 
-	std::cout << "The value of pi is: " << piValue << '\n';
+	printf("The value of π is:\e[0;32m %.32lf \e[0m\n", piValue);
 
 	return 0;
 }
