555bfb5a0294ddf9776416c0d3a6580f33da7d35
Druxorey <druxorey@gmail.com>
2025-02-05 22:21:16 -0400

    feat(c++): clean up and modularize S03[E10] pattern drawing code:
    - Extracted pattern drawing logic into separate functions
    - Updated main function to use new pattern functions
    - Improved code readability and maintainability

--- c++/S03-iterative-control-structures/E10-nested-squares.cpp
+++ c++/S03-iterative-control-structures/E10-nested-squares.cpp
@@ -1,70 +1,93 @@
 #include <iostream>
+#include "../U1-libraries/dxinput.cpp"
+
+void chessPattern(int borderSize) {
+	for (int i = 0; i < borderSize; i++) {
+
+		for (int j = 0; j < borderSize; j++) {
+			if (i % 2 == 0 || i == borderSize - 1 || j % 2 == 0 || j == borderSize - 1) {
+				std::cout << " x";
+			} else {
+				std::cout << "  ";
+			}
+		}
+		std::cout << '\n';
+	}
+}
+
+
+void dotsPattern(int borderSize) {
+	for (int i = 0; i < borderSize; i++) {
+
+		for (int j = 0; j < borderSize; j++) {
+			if (i == 0 || j == 0 || i == borderSize - 1 || j == borderSize - 1) {
+				std::cout << " x";
+			} else if (i % 2 == 0 && j % 2 == 0) {
+				std::cout << " o";
+			} else {
+				std::cout << "  ";
+			}
+		}
+		std::cout << '\n';
+	}
+}
+
+
+void fractalPattern(int borderSize) {
+	int isEven = ( ((borderSize - 1) / 2) % 2 == 0 ) ? 0 : 1;
+
+	for (int i = 0; i < borderSize; i++) {
+		int yDistance = abs(i - (borderSize - 1) / 2);
+
+		for (int j = 0; j < borderSize; j++) {
+			int xDistance = abs(j - (borderSize - 1) / 2);
+
+			if (i == 0 || j == 0 || i == borderSize - 1 || j == borderSize - 1) {
+				std::cout << " x";
+			} else if ((yDistance % 2 == isEven && yDistance >= xDistance) || (xDistance % 2 == isEven && xDistance >= yDistance)){
+				std::cout << " o";
+			} else {
+				std::cout << "  ";
+			}
+		}
+		std::cout << '\n';
+	}
+}
+
 
 int main(int argc, char *argv[]) {
-	std::cout << "\n\e[0;35m[========= NESTED SQUARE =========]\e[0m\n\n";
-
 	int squarePattern, borderSize;
 
-	std::cout << "1- Chess.\n2- Dots.\n3- Fractal.\n" << '\n';
-	std::cout << "Select the square pattern: ";
-	std::cin >> squarePattern;
-	
 	do {
-		std::cout << "Enter the border size [odd number]: ";
-		std::cin >> borderSize;
+		system("clear");
+		std::cout << "\n\e[0;35m[========= NESTED SQUARE =========]\e[0m\n\n";
+
+		std::cout << "  1 - Chess\n  2 - Dots\n  3 - Fractal\n" << '\n';
+		std::cout << "Select the square pattern: ";
+		getcin("", squarePattern);
+	
+		std::cout << "Enter the border size \e[0;33m[odd number]\e[0m: ";
+		getcin("", borderSize);
 	} while (borderSize % 2 == 0);
 	
 	std::cout << '\n';
 
-	if (squarePattern == 1){
-
-		for (int i = 0; i < borderSize; i++) {
-
-			for (int j = 0; j < borderSize; j++) {
-				if (i % 2 == 0 || i == borderSize - 1 || j % 2 == 0 || j == borderSize - 1) {
-					std::cout << " x";
-				} else {
-					std::cout << "  ";
-				}
-			}
-			std::cout << '\n';
-		}
-
-	} else if (squarePattern == 2) {
-
-		for (int i = 0; i < borderSize; i++) {
-
-			for (int j = 0; j < borderSize; j++) {
-				if (i == 0 || j == 0 || i == borderSize - 1 || j == borderSize - 1) {
-					std::cout << " x";
-				} else if (i % 2 == 0 && j % 2 == 0) {
-					std::cout << " o";
-				} else {
-					std::cout << "  ";
-				}
-			}
-			std::cout << '\n';
-		}
-
-	} else if (squarePattern == 3) {
-
-		for (int i = 0; i < borderSize; i++) {
-			int yDistance = abs(i - (borderSize - 1) / 2);
-
-			for (int j = 0; j < borderSize; j++) {
-				int xDistance = abs(j - (borderSize - 1) / 2);
-
-				if (i == 0 || j == 0 || i == borderSize - 1 || j == borderSize - 1) {
-					std::cout << " x";
-				} else if ((yDistance % 2 == 0 && yDistance >= xDistance) || (xDistance % 2 == 0 && xDistance >= yDistance)){
-					std::cout << " o";
-				} else {
-					std::cout << "  ";
-				}
-			}
-			std::cout << '\n';
-		}
+	switch (squarePattern) {
+		case 1:
+			chessPattern(borderSize);
+			break;
+		case 2:
+			dotsPattern(borderSize);
+			break;
+		case 3:
+			fractalPattern(borderSize);
+			break;
+		default:
+			std::cout << "Invalid option.";
+			break;
 	}
+
+	std::cout << '\n';
 
 	return 0;
 }
