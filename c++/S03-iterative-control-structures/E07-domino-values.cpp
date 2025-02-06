e06a9bdf4a5d578e0cad6e14b7cccbb0c404369b
Druxorey <druxorey@gmail.com>
2025-02-05 21:15:16 -0400

    style(c++): change S03[E07] domino values to white background, black text.

--- c++/S03-iterative-control-structures/E07-domino-values.cpp
+++ c++/S03-iterative-control-structures/E07-domino-values.cpp
@@ -3,10 +3,28 @@
 int main(int argc, char *argv[]) {
 	std::cout << "\n\e[0;35m[========= DOMINO VALUES =========]\e[0m\n\n";
 
+	int printLimit = 0;
+
 	for (int i = 0; i <= 6; i++) {
 		for (int j = 0; j <= i; j++) {
-			std::cout << i << ":" << j << '\n';
+			// Print the initial separator for each row
+			if (printLimit == 0) {
+				std::cout << "   ";
+			}
+
+			// Print the domino values
+            std::cout << " \e[47;30m" << i << "|" << j << "\e[0m";
+			printLimit++;
+
+			// If the limit is reached, print a new line
+			if (printLimit == 7) {
+				std::cout << "\n";
+				printLimit = 0;
+			}
 		}
 	}
+
+	std::cout << "\n";
+
 	return 0;
 }
