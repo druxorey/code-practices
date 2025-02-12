1718908a79d9b96dc5ee90935bf0ca1a340cb8c1
Druxorey <druxorey@gmail.com>
2025-02-12 17:49:12 -0400

    feat(libraries): add dxlist print, fill, and rand methods.

--- c++/U1-libraries/dxlist.cpp
+++ c++/U1-libraries/dxlist.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
 #include <list>
 
+template <typename datatype>
+class dxlist : public std::list<datatype> {
+	public:
+		void print();
+		void fill(int size, int initialValue = 0);
+		void rand(int size);
+};
+
 // Print the contents of the list using the STD implementation
 template <typename datatype>
-void printList(const std::list<datatype> &myList) {
+void dxlist<datatype>::print() {
     int nodeIndex = 1;
-    for (const auto &item : myList) {
-        std::cout << "Node " << nodeIndex << ": [" << item << "]" << std::endl;
+    for (auto iterator = this->begin(); iterator != this->end(); ++iterator) {
+        std::cout << "Node " << nodeIndex << ": [" << *iterator << "]" << std::endl;
         nodeIndex++;
     }
+}
+
+// Fill the list with values
+template <typename datatype>
+void dxlist<datatype>::fill(int size, int initialValue) {
+    for (int i = initialValue; i < size + initialValue; i++) {
+		this->push_back(i);
+	}
+}
+
+// Fill the list with random values
+template <typename datatype>
+void dxlist<datatype>::rand(int size) {
+	srand(time(NULL));
+    for (int i = 0; i < size; i++) {
+		this->push_back(::rand() % size);
+	}
 }
