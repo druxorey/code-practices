269c71364b56fbbb3de0a24df33609e434e6a98a
Druxorey <druxorey@gmail.com>
2025-02-12 15:21:38 -0400

    refactor(libraries): separate ring class method implementations.

--- c++/U1-libraries/dxring.cpp
+++ c++/U1-libraries/dxring.cpp
@@ -3,37 +3,51 @@
 
 template <typename datatype>
 class ring : public std::list<datatype> {
-public:
-    ring() : it(this->begin()) {}
+	public:
+		ring();
 
-    void add(const datatype& value) {
-        this->push_back(value);
-        if (this->size() == 1) {
-            it = this->begin();
-        }
-    }
+		void add(const datatype& value);
+		void remove();
+		datatype& next();
 
-    void remove() {
-        if (!this->empty()) {
-            it = this->erase(it);
-            if (it == this->end() && !this->empty()) {
-                it = this->begin();
-            }
-        }
-    }
-
-    datatype& next() {
-        if (this->empty()) {
-            throw std::out_of_range("List is empty");
-        }
-        if (it == this->end()) {
-            it = this->begin();
-        }
-        datatype& value = *it;
-        ++it;
-        return value;
-    }
-
-private:
-    typename std::list<datatype>::iterator it;
+	private:
+		typename std::list<datatype>::iterator it;
 };
+
+// Constructor: Initializes an empty ring
+template <typename datatype>
+ring<datatype>::ring() : it(this->begin()) {}
+
+// Allows the user to add a new value to the ring
+template <typename datatype>
+void ring<datatype>::add(const datatype& value) {
+	this->push_back(value);
+	if (this->size() == 1) {
+		it = this->begin();
+	}
+}
+
+// Allows the user to remove the current value from the ring
+template <typename datatype>
+void ring<datatype>::remove() {
+	if (!this->empty()) {
+		it = this->erase(it);
+		if (it == this->end() && !this->empty()) {
+			it = this->begin();
+		}
+	}
+}
+
+// Allows the user to get the next value in the ring in a circular way
+template <typename datatype>
+datatype& ring<datatype>:: next() {
+	if (this->empty()) {
+		throw std::out_of_range("List is empty");
+	}
+	if (it == this->end()) {
+		it = this->begin();
+	}
+	datatype& value = *it;
+	++it;
+	return value;
+}
