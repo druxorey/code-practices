c6716aba5d38098abf6a23bca9f20618a4b1355f
Druxorey <druxorey@gmail.com>
2025-02-12 13:59:23 -0400

    feat(libraries): added circular list.

---
+++ c++/U1-libraries/dxcircular_list.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <list>
+
+template <typename datatype>
+class circularList : public std::list<datatype> {
+public:
+    circularList() : it(this->begin()) {}
+
+    void add(const datatype& value) {
+        this->push_back(value);
+        if (this->size() == 1) {
+            it = this->begin();
+        }
+    }
+
+    void remove() {
+        if (!this->empty()) {
+            it = this->erase(it);
+            if (it == this->end() && !this->empty()) {
+                it = this->begin();
+            }
+        }
+    }
+
+    datatype& next() {
+        if (this->empty()) {
+            throw std::out_of_range("List is empty");
+        }
+        if (it == this->end()) {
+            it = this->begin();
+        }
+        datatype& value = *it;
+        ++it;
+        return value;
+    }
+
+private:
+    typename std::list<datatype>::iterator it;
+};
