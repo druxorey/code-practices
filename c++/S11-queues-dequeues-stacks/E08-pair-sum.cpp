0d83b3b1de51df954c8174add715355fbf154395
Druxorey <druxorey@gmail.com>
2025-02-13 18:26:08 -0400

    feat(c++): added S11[E08] exercise.

---
+++ c++/S11-queues-dequeues-stacks/E08-pair-sum.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "../U1-libraries/dxstack.cpp"
+#include "../U1-libraries/dxinput.cpp"
+
+dxstack<int> getPairs(dxstack<int> stack) {
+	dxstack<int> temporalStack;
+	int sum;
+
+	while (!stack.empty()) {
+		if (stack.size() == 1) {
+			temporalStack.push(stack.top());
+			stack.pop();
+			break;
+		}
+
+		sum = stack.top();
+		stack.pop();
+		sum += stack.top();
+		stack.pop();
+
+		temporalStack.push(sum);
+	}
+
+	while (!temporalStack.empty()) {
+		stack.push(temporalStack.top());
+		temporalStack.pop();
+	}
+
+	return stack;
+}
+
+
+int main(int argc, char *argv[]) {
+	std::cout << "\n\e[0;35m[========= PAIR SUM =========]\e[0m\n\n";
+
+	int stackSize;
+	getcin("Enter the size of the stack: ", stackSize);
+
+	dxstack<int> stack;
+	stack.rand(stackSize);
+
+	printf("\n\e[0;34mInitial stack: \e[0m\n\n");
+	printStack(stack);
+
+	dxstack<int> pairStack = getPairs(stack);
+	printf("\n\e[0;34mPair stack: \e[0m\n\n");
+	printStack(pairStack);
+
+	return 0;
+}
