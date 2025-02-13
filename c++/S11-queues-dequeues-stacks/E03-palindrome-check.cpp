c2152d8bb4cf6a1c5b9c3ec2aa4393844fe8f9c5
Druxorey <druxorey@gmail.com>
2025-02-13 00:18:00 -0400

    feat(c++): added S11[E03] exercise.

---
+++ c++/S11-queues-dequeues-stacks/E03-palindrome-check.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "../U1-libraries/dxstack.cpp"
+#include "../U1-libraries/dxinput.cpp"
+
+dxstack<char> strToStack(std::string str) {
+	dxstack<char> stack;
+	for(const char &c : str) stack.push(c);
+	return stack;
+}
+
+
+bool isPalindrome(dxstack<char> stack) {
+	int size = stack.size();
+	dxstack<char> fistHalf;
+
+	for (int i = 0; i < size / 2; i++) {
+		fistHalf.push(stack.top());
+		stack.pop();
+	}
+
+	if (size % 2 != 0) stack.pop();
+
+	for (int i = 0; i < size / 2; i++) {
+		if (fistHalf.top() != stack.top()) return false;
+		fistHalf.pop();
+		stack.pop();
+	}
+
+	return true;
+}
+
+
+int main(int argc, char *argv[]) {
+	std::cout << "\n\e[0;35m[========= PALINDROME CHECK =========]\e[0m\n\n";
+
+	std::string word;
+	getcin("Enter a word: ", word);
+
+	dxstack<char> stack = strToStack(word);
+
+	if (isPalindrome(stack))
+		printf("\n\e[0;32mThe word \"%s\" is a palindrome\e[0m\n\n", word.c_str());
+	else
+		printf("\n\e[0;31mThe word \"%s\" is not a palindrome\e[0m\n\n", word.c_str());
+
+	return 0;
+}
