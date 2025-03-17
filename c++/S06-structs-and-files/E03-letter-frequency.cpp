d3e907180d57e64258d783c8f8676e18bc31349a
Druxorey <druxorey@gmail.com>
2025-03-17 19:14:57 -0400

    feat(c++): add S06-E03 full alphabet frequency analysis

--- c++/S06-structs-and-files/E03-letter-frequency.cpp
+++ c++/S06-structs-and-files/E03-letter-frequency.cpp
@@ -1,33 +1,60 @@
 #include <iostream>
 #include <fstream>
+#include "../U1-libraries/dxarray.hpp"
+
+const int MAX_LETTERS = 26;
+const char letters[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+						  'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+						  'u', 'v', 'w', 'x', 'y', 'z'};
+
+void isArgumentValid(int argc, char *argv[]) {
+	if (argc != 2) {
+		std::cerr << "\e[0;31m[ERROR]\e[0m Usage: " << argv[0] << " <filename>\n";
+		exit(1);
+	}
+}
+
+
+void getFrequency(std::ifstream &file, int frequency[], int &other) {
+	char actualLetter;
+
+	initArray(frequency, MAX_LETTERS, 0);
+
+	while (file.get(actualLetter)) {
+		if (actualLetter >= 'a' && actualLetter <= 'z') {
+			frequency[actualLetter - 'a']++;
+		} else if (actualLetter >= 'A' && actualLetter <= 'Z') {
+			frequency[actualLetter - 'A']++;
+		} else {
+			if (actualLetter != ' ' && actualLetter != '\n') other++;
+		}
+	}
+	other--; // This is because the last character is not a letter
+}
+
+
+void printFrequency(int frequency[], int others) {
+    printf("\e[0;34m[INFO]\e[0m Letter frequencies:\n\n");
+    for (int i = 0 ; i < MAX_LETTERS; i++) {
+        printf("    \e[0;33m[\e[0m %c \e[0;33m]\e[0m - %d\n", letters[i], frequency[i]);
+    }
+    printf("\n\e[0;33m[INFO]\e[0m Other characters - %d\n", others);
+}
+
 
 int main(int argc, char *argv[]) {
 	std::cout << "\n\e[0;35m[========= LETTER FREQUENCY =========]\e[0m\n\n";
 
-	char letters[] = {'e', 'a', 'o', 'i', 'u'};
-	char mayus[] = {'E', 'A', 'O', 'I', 'U'};
-	int frequency[] = {0, 0, 0, 0, 0};
-	char actualLetter;
+	isArgumentValid(argc, argv);
 
-	std::cout << "Enter the path of the file: ";
-	std::string path;
-	std::cin >> path;
+	std::ifstream file(argv[1]);
 
-	std::ifstream file(path);
-
-	while (file.get(actualLetter)) {
-		for (int i = 0 ; i < 5; i++) {
-			if (actualLetter == letters[i] or actualLetter == mayus[i]) {
-				frequency[i]++;
-			}
-		}
-	}
+	int frequency[MAX_LETTERS];
+	int others = 0;
+	getFrequency(file, frequency, others);
+	printFrequency(frequency, others);
 
 	file.close();
-
-	for (int i = 0 ; i < 5; i++) {
-		std::cout << letters[i] << "-" << frequency[i] << '\n';
-	}
 
 	return 0;
 }
