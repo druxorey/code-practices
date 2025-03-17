12a1b8e80503ab73a60441ed6d8ad354fa6e674d
Druxorey <druxorey@gmail.com>
2025-03-17 18:39:13 -0400

    chore(c++): improve S06-E02 point distance calculation logic

--- c++/S06-structs-and-files/E02-point-distance.cpp
+++ c++/S06-structs-and-files/E02-point-distance.cpp
@@ -1,37 +1,36 @@
 #include <iostream>
 #include <cmath>
-#include "../U1-libraries/dxinput.hpp"
 
-struct dot {
+const int MAX_POINTS = 2;
+
+struct point {
 	float x = 0;
 	float y = 0;
 	float z = 0;
 };
 
 
+point getPoint(int index) {
+	point p;
+	printf("Enter the \e[0;33m[x y z]\e[0m coordinates of the point \e[0;33m%d\e[0m: ", index + 1);
+	std::cin >> p.x >> p.y >> p.z;
+	return p;
+}
+
+
+float getDistance(point p1, point p2) {
+	return sqrt(pow((p1.x - p2.x),2) + pow((p1.y - p2.y),2) + pow((p1.z - p2.z),2));
+}
+
+
 int main(int argc, char *argv[]) {
 	std::cout << "\n\e[0;35m[========= POINT DISTANCE =========]\e[0m\n\n";
 
-	dot dots[2];
+	point points[MAX_POINTS];
+	for (int i = 0; i < MAX_POINTS; i++) points[i] = getPoint(i);
 
-	for (int i = 0; i < 2; i++) {
-		int coorditate;
-		std::cout << "Enter coorditates for the dot number " << i + 1 << ": " << '\n';
-		std::cin >> coorditate;
-		dots[i].x = coorditate;
-		std::cin >> coorditate;
-		dots[i].y = coorditate;
-		std::cin >> coorditate;
-		dots[i].z = coorditate;
-	}
-
-	float firstCalc, secondCalc, thirdCalc;
-
-	firstCalc =  pow((dots[0].x - dots[1].x),2);
-	secondCalc =  pow((dots[0].y - dots[1].y),2);
-	thirdCalc =  pow((dots[0].z - dots[1].z),2);
-
-	std::cout << "The distance between the points is: " << sqrt(firstCalc + secondCalc + thirdCalc) << '\n';
+	float distance = getDistance(points[0], points[1]);
+	printf("\nThe distance between the points is: \e[0;32m%.2f\e[0m\n\n", distance);
 
 	return 0;
 }
