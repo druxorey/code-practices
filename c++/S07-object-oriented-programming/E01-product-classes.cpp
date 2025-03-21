fa23c8eb5541f9d9f3ae6a33314deac5b3b524f8
Druxorey <druxorey@gmail.com>
2025-03-21 14:18:36 -0400

    feat(c++): add S07-E01 dynamic product creation and review functionality

--- c++/S07-object-oriented-programming/E01-product-classes.cpp
+++ c++/S07-object-oriented-programming/E01-product-classes.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
+#include "../U1-libraries/dxinput.hpp"
 
 class products {
-	protected:
-		float iva;
-		float discount;
 	public:
-		int code;
 		std::string name;
-		float price = 0;
+		int code;
 
-		products(float iva, float discount) : iva(iva), discount(discount) {}
+		products(float price, std::string name, int code) {
+			this->price = price;
+			this->name = name;
+			this->code = code;
+		}
+
+		void setIva(float iva) {
+			this->iva = iva; 
+		}
+
+		float getPrice() {
+			return price + ( price * iva / 100);
+		}
+
+		void setPrice(float price) {
+			this->price = price;
+		}
+
+	private:
+		float price;
+		float iva = 0;
 };
+
 
 class book: public products {
 	public:
@@ -20,26 +38,95 @@ class book: public products {
 		int year;
 		int isbn;
 
-		book() : products(10, 20) {}
-
-		float getPrice() {
-			return price + ( price * iva /100) - (price * discount / 100);
-		}
+		book(float price, std::string name, int code): products(price, name, code) {}
 };
+
 
 class cd: public products {
 	public:
 		std::string interpret;
 		std::string title;
 		int year;
+
+		cd(float price, std::string name, int code): products(price, name, code) {}
 };
 
-int main(int argc, char *argv[]) {
-	std::cout << "\n\e[0;35m[========= PRODUCT CLASSES =========]\e[0m\n\n";
+products* createBook() {
+    std::string name;
+    int code;
+    float price;
 
-	book b;
-	b.code = 1;
-	b.price = 20;
-	std::cout << b.code << ";" << b.getPrice() << '\n';
+    getcin("Enter the name: ", name);
+    getcin("Enter the price: ", price);
+    getcin("Enter the code: ", code);
+
+    return new book(price, name, code);
+}
+
+products* createCD() {
+    std::string name;
+    int code;
+    float price;
+
+    getcin("Enter the name: ", name);
+    getcin("Enter the price: ", price);
+    getcin("Enter the code: ", code);
+
+    return new cd(price, name, code);
+}
+
+
+void reviewProduct(products* product) {
+    if (product == nullptr) {
+        printf("No product has been created.\n");
+    } else {
+        printf("Name: %s\n", product->name.c_str());
+        printf("Code: %d\n", product->code);
+        printf("Price: %.2f\n", product->getPrice());
+    }
+
+	enter();
+}
+
+
+int main(int argc, char *argv[]) {
+    products* product = nullptr;
+    char option;
+
+    do {
+		system("clear");
+
+		std::cout << "\n\e[0;35m[========= PRODUCT CLASSES =========]\e[0m\n\n";
+
+        printf("Select an option:\n");
+        printf("1. Create book\n");
+        printf("2. Create CD\n");
+        printf("3. Review product\n");
+        printf("4. Exit\n");
+
+        getcin("\nOption: ", option);
+
+		system("clear");
+
+        switch(option) {
+            case '1':
+                product = createBook();
+                break;
+            case '2':
+                product = createCD();
+                break;
+            case '3':
+                reviewProduct(product);
+                break;
+            case '4':
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid option.\n");
+        }
+    } while (option != '4');
+
+    delete product;
+
 	return 0;
 }
